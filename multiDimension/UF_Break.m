3cdd6f1b2c0dde0f09b78769285a7f881c7c7ef6
Zhou YU <laysent@gmail.com>
2012-09-24 07:38:57 +0800

    The Top of any Union should NOT be motified as it has the greatest 'Food'

--- multiDimension/UF_Break.m
+++ multiDimension/UF_Break.m
@@ -1,18 +1,18 @@
 function [list] = UF_Break(list, fish, p)
   if list(p) == p
-    max = -Inf;
-    maxIter = -1;
-    for i = 1:size(list)(2)
-      if i != p && list(i) == p && max < getFood(fish(i,:))
-	max = getFood(fish(i,:));
-	maxIter = i;
-      endif
-    endfor
-    for i = 1:size(list)(2)
-      if i != p && list(i) == p
-	list(i) = maxIter;
-      endif
-    endfor
+%    max = -Inf;
+%    maxIter = -1;
+%    for i = 1:size(list)(2)
+%      if i != p && list(i) == p && max < getFood(fish(i,:))
+%	max = getFood(fish(i,:));
+%	maxIter = i;
+%      endif
+%    endfor
+%    for i = 1:size(list)(2)
+%      if i != p && list(i) == p
+%	list(i) = maxIter;
+%      endif
+%    endfor
   else
     list(p) = p;
   endif
