1f287ff77d8e4a6fa241fe98ba251e67fbde00fd
Leon Zhou <the.reason.sake@gmail.com>
2012-11-18 19:15:58 +0800

    rm all

--- multiDimension/UF.m
+++
@@ -1,6 +0,0 @@
-function [list] = UF_Create(N)
-  list = zeros(1,N);
-  for i = 1:N
-    list(i) = i;
-  endfor
-endfunction
--- multiDimension/UF_Connected.m
+++
@@ -1,7 +0,0 @@
-function [boolean] = UF_Connected(list, p, q)
-  if UF_Find(list, p) == UF_Find(list, q)
-    boolean = 1;
-  else
-    boolean = 0;
-  endif
-endfunction
