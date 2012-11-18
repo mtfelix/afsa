dcc613ab099f2e8b8f478df3b8a804d82d3cae86
LaySent <laysent@gmail.com>
2012-11-18 19:38:58 +0800

    new file:   UF_Create.m 	new file:   kMeanFunction.m

---
+++ kMeanFunction.m
@@ -0,0 +1,37 @@
+## Copyright (C) 2012 reAsOn
+## 
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with Octave; see the file COPYING.  If not, see
+## <http://www.gnu.org/licenses/>.
+
+## kMeanFunction
+
+## Author: reAsOn <reason@For-Napkin>
+## Created: 2012-08-12
+
+function [ ret ] = kMeanFunction (distance)
+%    ret = 1 ./ (1 + exp(1*distance-5));
+
+%  ret = 0;
+%  for i = 1:size(distance)(1)
+%    tmp = sqrt(distance(i,:).^2);
+%    if tmp <= 1
+%      ret += (1 - tmp / 1);
+%    endif
+%  endfor
+
+  ret = exp(-0.5 * sum(distance.^2, 2));
+
+%    ret = 1 ./ (1 + exp(1*abs(distance)));
+%    ret = 1 ./ (1 + exp(1*abs(distance)));
+endfunction
